df218b5e8bfddceba54d77fedc439aae5707b2ea
Maciek <msajdera@gmail.com>
2023-03-01 21:14:24 +0100

    Side menu for all pages.

--- header.php
+++ header.php
@@ -24,49 +24,14 @@
 
     <div id="progressBar"></div>
 
-    <!-- all pages  -->
-
-    <?php if (!is_front_page()): ?>
-
-    <header class="fixed w-full z-40">
-        <div class="mx-auto container flex">
-
-            <?php do_action( 'embepiercing_header' ); ?>
-            <?php get_template_part( 'template-parts/layout/menu-mobile'); ?>
-            <?php get_template_part( 'template-parts/layout/menu-desktop'); ?>
-
-        </div>
-    </header>
-
-    <div id="heroImageHolder" class="embebg fadeOut -z-10">
-
-    <?php $front_page_id = get_option( 'page_on_front' ); ?>
-    <?php $welcome_view_image = get_field('welcome_view_image', $front_page_id); ?>
-    <img src="<?php echo $welcome_view_image['url']; ?>" alt="<?php echo $welcome_view_image['alt']; ?>" />
-
-    </div>
-
-    <?php endif; ?>
-
-    <!-- front page only -->
-
-    <?php if (is_front_page()): ?>
-
-        <?php echo get_template_part('/template-parts/layout/menu-desktop-front-page_1'); ?>
-
-    <?php endif; ?>
+    <?php echo get_template_part('/template-parts/layout/menu-desktop-front-page_1'); ?>
 
     <div id="luxy" class="z-30 will-change-transform">
 
         <div id="page" class="min-h-screen flex flex-col opacity-0 transition-opacity duration-300">
             
-            <!-- front page only -->
-
-            <?php if (is_front_page()): ?>
             <?php get_template_part( 'template-parts/layout/menu-mobile'); ?>
             <?php echo get_template_part('/template-parts/layout/menu-desktop-front-page_2'); ?>
-
-            <?php endif; ?>
 
             <div id="content" class="site-content pt-20 md:pt-40 flex-grow">
 
